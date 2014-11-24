586fca656048cfba68ffb55c3deec8914930acb8
Arnaud Wetzel <arnaud.wetzel@kbrwadventure.com>
2014-11-25 00:05:38 +0100

    add package desc for hex

--- mix.exs
+++ mix.exs
@@ -4,11 +4,26 @@ defmodule Rulex.Mixfile do
   def project do
     [app: :rulex,
      version: "0.0.1",
-     elixir: "~> 0.15.0",
+     elixir: "~> 1.0.0",
+     description: description,
+     package: package,
      deps: []]
   end
 
   def application do
     [applications: []]
+  end
+
+  defp package do
+    [ contributors: ["Arnaud Wetzel"],
+      licenses: ["The MIT License (MIT)"],
+      links: %{ "GitHub"=>"https://github.com/awetzel/rulex" } ]
+  end
+
+  defp description do
+    """
+    Rulex contains a very simple macro "defrule" allowing you to write a rule
+    system using Elixir pattern matching.
+    """
   end
 end
